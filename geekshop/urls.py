2e694e1e5b96d6d5d9bed6e3b7db81f68877ff24
Ramilia <rd.rd@mail.ru>
2021-02-04 16:18:34 +0300

    lesson_4

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -20,11 +20,11 @@ from django.conf.urls.static import static
 
 from mainapp.views import index
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', include('mainapp.urls', namespace='products')),
+    path('auth/', include('authapp.urls', namespace='auth')),
 ]
 
 if settings.DEBUG:
