cb26305a10819214f0190e2144622d10ad0d2bd5
Ramilia <rd.rd@mail.ru>
2021-02-17 16:28:18 +0300

    added scrolling by pages and categories

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import datetime
 from mainapp.models import ProductCategory, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
@@ -11,10 +12,37 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request, id=None):
+# def products(request, category_id=None):
+#     context = {
+#         'title': 'каталог',
+#         'categories': ProductCategory.objects.all(),
+#     }
+#     if category_id:
+#         products = Product.objects.filter(category_id=category_id)
+#     else:
+#         products = Product.objects.all()
+#     #вариант оптимизации:
+#     #products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#     context.update({'products': products})
+#     return render(request, 'mainapp/products.html', context)
+
+def products(request, category_id=None, page=1):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id).order_by('price')
+    else:
+        products = Product.objects.all().order_by('price') #"-price"  - обратная сортировка
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page)
     context = {
         'title': 'каталог',
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all()
+        'categories': ProductCategory.objects.all(),
+        'products': products_paginator,
     }
     return render(request, 'mainapp/products.html', context)
+    # try:
+    #     products_paginator = paginator.page(page)
+    # except PageNotAnInteger:
+    #     products_paginator = paginator.page(1)
+    # except EmptyPage:
+    #     products_paginator = paginator.page(paginator.num_pages)
